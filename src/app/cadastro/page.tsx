f915c67e7fdff62e48c3b7135b75f58f1d73d798
dinizdev <dinizdevmaster@gmail.com>
2025-04-06 06:50:12 +0200

    fix login design

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -5,8 +5,10 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { supabase } from '@/lib/supabaseClient'
 import { Loader2 } from 'lucide-react'
+import Footer from '@/components/footer'
+import MenuBar from '@/components/menu-bar'
 
-export default function Signup() {
+export default function Signup(className: any) {
   const [email, setEmail] = useState('')
   const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState('')
@@ -30,42 +32,46 @@ export default function Signup() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900">
-      <Card className="w-full max-w-md p-6 bg-gray-800 text-white shadow-xl">
-        <CardHeader>
-          <CardTitle className="text-center text-2xl font-bold">
-            Cadastro
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSignup} className="space-y-4">
-            <Input
-              type="email"
-              placeholder="Digite seu e-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="bg-gray-700 border-gray-600 focus:ring-orange-500"
-            />
-            <Button
-              type="submit"
-              className="w-full bg-orange-600 hover:bg-orange-700"
-              disabled={loading}
-            >
-              {loading ? (
-                <Loader2 className="animate-spin" size={20} />
-              ) : (
-                'Entrar com Magic Link'
-              )}
-            </Button>
-          </form>
-          {message && (
-            <p className="mt-4 text-center text-sm text-orange-400">
-              {message}
-            </p>
-          )}
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      <MenuBar />
+      <div className="flex items-center flex-col justify-center bg-zinc-950 min-h-screen">
+        <Card className="w-full max-w-md p-6 text-white border-none bg-zinc-950 ">
+          <CardHeader>
+            <CardTitle className="text-center text-3xl font-bold">
+              Cadastro
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSignup} className="space-y-4">
+              <Input
+                type="email"
+                placeholder="Digite seu e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="bg-zinc-900 border-gray-600 text-center  focus:ring-orange-500"
+              />
+              <Button
+                type="submit"
+                className="w-full h-14 bg-orange-700 text-md font-bold hover:bg-orange-700"
+                disabled={loading}
+              >
+                {loading ? (
+                  <Loader2 className="animate-spin" size={20} />
+                ) : (
+                  'Entrar com Magic Link'
+                )}
+              </Button>
+            </form>
+            {message && (
+              <p className="mt-4 text-center text-sm text-green-400">
+                {message}
+              </p>
+            )}
+          </CardContent>
+        </Card>
+        <Footer className={` ${className}`} />
+      </div>
+    </>
   )
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,13 @@
-export default function Footer() {
+interface FooterProps {
+  className?: string
+}
+
+export default function Footer({ className }: FooterProps) {
   return (
     <div>
-      <footer className="bg-slate-950 text-white text-center p-6">
+      <footer
+        className={`bg-slate-950 text-white text-center p-6 ${className}`}
+      >
         <p>© 2025 Trim-ly | Feito no Brasil com Amor e Carinho. 🇧🇷</p>
       </footer>
     </div>
